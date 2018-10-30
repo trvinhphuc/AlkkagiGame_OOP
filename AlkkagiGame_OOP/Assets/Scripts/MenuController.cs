54db5ca700f867ea17f530f4c053476ad583374c
Truong Vinh Phuc <51303078@hcmut.edu.vn>
2018-10-30 21:09:49 +0700

    initial commit

---
+++ AlkkagiGame_OOP/Assets/Scripts/MenuController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour {
+
+	public void MenuGame()
+	{
+		Application.LoadLevel("Menu_scene");
+
+	}
+	public void StartGame()
+	{
+		Application.LoadLevel("Start_scene");
+
+	}
+	public void OptionMenu()
+	{
+		Application.LoadLevel("Option_scene");
+	}
+	public void AboutMenu()
+	{
+		Application.LoadLevel("About_scene");
+	}
+
+	public void How2playMenu()
+	{
+		Application.LoadLevel("How2play_scene");
+	}
+
+	public void EndGame ()
+	{
+		Application.Quit ();
+	}
+}
